f2b48454d0b865e8f7157af816f9b6b7eb8baeea
刘佳 <liujiahenan@gmail.com>
2015-11-23 22:27:58 +0800

    add MyRealm2.java and MyRealm3.java

---
+++ src/main/java/com/mcliu/shiro/realm/MyRealm2.java
@@ -0,0 +1,36 @@
+package com.mcliu.shiro.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.Realm;
+
+public class MyRealm2 implements Realm {
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String)token.getPrincipal();
+        String password = new String((char[])token.getCredentials());
+        if (!"jinhua".equals(username)) {
+            throw new UnknownAccountException();
+        }
+        if (!"123".equals(password)) {
+            throw new IncorrectCredentialsException();
+        }
+        // 如果身份认证验证成功，返回一个AuthenticationInfo实现  
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+
+    public String getName() {
+        return "myrealm2";
+    }
+
+    public boolean supports(AuthenticationToken token) {
+        // 仅支持UsernamePasswordToken类型的Token  
+        return token instanceof UsernamePasswordToken;
+    }
+
+}
---
+++ src/main/java/com/mcliu/shiro/realm/MyRealm3.java
@@ -0,0 +1,36 @@
+package com.mcliu.shiro.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.realm.Realm;
+
+public class MyRealm3 implements Realm {
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String)token.getPrincipal();
+        String password = new String((char[])token.getCredentials());
+        if (!"zhang".equals(username)) {
+            throw new UnknownAccountException();
+        }
+        if (!"123".equals(password)) {
+            throw new IncorrectCredentialsException();
+        }
+        // 如果身份认证验证成功，返回一个AuthenticationInfo实现  
+        return new SimpleAuthenticationInfo(username + "@163.com", password, getName());
+    }
+
+    public String getName() {
+        return "myrealm3";
+    }
+
+    public boolean supports(AuthenticationToken token) {
+        // 仅支持UsernamePasswordToken类型的Token  
+        return token instanceof UsernamePasswordToken;
+    }
+
+}
